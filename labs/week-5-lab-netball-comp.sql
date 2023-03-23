31db39afd33c24017779b3b1d311e1ccca817332
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-03-24 12:14:32 +1300

    Create table for team_member

--- labs/week-5-lab-netball-comp.sql
+++ labs/week-5-lab-netball-comp.sql
@@ -3,7 +3,7 @@ CREATE DATABASE netball_competition
 USE netball_competition;
 
 CREATE TABLE person (
-	person_id SMALLINT IDENTITY PRIMARY KEY,
+	person_id INT IDENTITY PRIMARY KEY,
 	first_name VARCHAR(20),
 	last_name VARCHAR(40)
 	);
@@ -22,4 +22,10 @@ CREATE TABLE team (
 	FOREIGN KEY (club_name) REFERENCES club(club_name)
 	);
 
+CREATE TABLE team_member (
+	person_id INT FOREIGN KEY REFERENCES person(person_id),
+	team_name VARCHAR(50) FOREIGN KEY REFERENCES team(team_name),
+	PRIMARY KEY (person_id, team_name)
+	position VARCHAR(2) -- Is 2 because netball positions are GK, GD, WD, WA, C, GA and GS
+	);
 
