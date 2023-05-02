b807be5c7fb9e3487d5d828063cde60df75061d9
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-05-03 08:28:21 +1200

    Completed exersise 1

--- wk9.1-notes.sql
+++ wk9.1-notes.sql
@@ -19,3 +19,13 @@ SELECT column_name + 1000 AS 'Column Name'
 
 SELECT album_name, number_of_sales, number_of_sales + 100 AS 'Number of Sales Plus 100'
     FROM album;
+
+-- Class Exersise 1
+
+/* Write a query that adds an album track's track_order to the producer id of the related album.
+    Also give the track names in the output, include all album tracks in the result */
+
+SELECT t.track_name, t.track_order, a.producer_id, t.track_order + a.producer_id AS 'Track Order Plus Producer ID' FROM album_track t
+    INNER JOIN album a
+        ON t.album_name = a.album_name
+        AND t.artist_id = a.artist_id;
