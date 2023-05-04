d7fdfaa2cfdec1ce103e69b160cee4301922d52b
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-05-05 08:15:41 +1200

    UNION notes

--- wk9.2-notes.sql
+++ wk9.2-notes.sql
@@ -8,3 +8,18 @@
         - EXCEPT
 */
 
+-- 1. UNION
+-- Combines the results of two or more SELECT statements into a single result set that includes all the rows that belong to all SELECT statements in the union.
+
+-- Basic Syntax
+SELECT column_name1 FROM table1
+UNION
+SELECT column_name2 FROM table2;
+
+-- Example from 100 Albums Database
+-- Get the names of all artists and producers in the database.
+SELECT name FROM artist
+UNION
+SELECT name FROM producer;
+
+-- This will return a list of all the names of artists and producers in the database. It will also remove any duplicates if there is someone who is both an artist and a producer.
